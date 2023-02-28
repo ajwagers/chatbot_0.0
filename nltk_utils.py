af9b3c4729539617b1fcc96dfc30a6908d394e22
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2023-02-27 22:04:50 -0500

    Update nltk_utils.py

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,11 @@
 # nltk_utils.py
+# This file preps the data to be used by the model.  
+# First, the sentence is split into words or characters (tokenized).
+# Second, the word are reduced to their "stem" by chopping the end of the words off.  
+#    This allows us to match roots of words rather than each different form of the word.
+# Third, the tokenized sentence is translated into a vector of 0's and 1's based on a "bag of words".
+# The next step is to feed this vector to the model and receive the results.
+
 import numpy as np
 import nltk
 from nltk.stem.porter import PorterStemmer
@@ -27,6 +34,10 @@ def bag_of_words(tokenized_sentence, words):
     """
     return bag of words array:
     1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
     # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
